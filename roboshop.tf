a83e56a21b98f1f7db53fde5189cc4fa04dd4df7
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-20 21:40:14 +0530

    okko

--- roboshop.tf
+++ roboshop.tf
@@ -1,7 +1,7 @@
 
 
 module "roboshop" {
-    source = "git::https://github.com/roboshop-project-v1/tf-modules-vpc.git"
+    source = "git::https://github.com/roboshop-project-v1/tf-module-vpc"
 
     for_each = var.vpc
     cidr = each.value["cidr"]
