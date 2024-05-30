53cfbb73210855f1529c5f763ddce6026f459667
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-30 22:05:30 +0530

    okok

--- localsvars.tf
+++ localsvars.tf
@@ -3,6 +3,6 @@ locals {
   app_subnets = [for k,v in lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),"app",null),"name",null) : v.id]
   db_subnets = [for k,v in lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),"db",null),"name",null) : v.id]
   
-  app_subnets_cidr = [for k,v in lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),"app",null),"name",null) : v.cidr_blocks]
+  app_subnets_cidr = [for k,v in lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),"app",null),"name",null) : v.cidr_block]
   
 }
