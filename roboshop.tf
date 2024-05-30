49ffca6482afb7560563d73a1c322cf915a05c04
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-31 00:53:56 +0530

    okok

--- roboshop.tf
+++ roboshop.tf
@@ -15,20 +15,20 @@ module "vpc" {
 
 
 
-# module "alb" {
-#     source = "git::https://github.com/roboshop-project-v1/tf-module-alb"
+module "alb" {
+    source = "git::https://github.com/roboshop-project-v1/tf-module-alb"
 
-#     for_each = var.alb
-#     env = var.env
-#     alb_type = each.value["lb_type"]
-#     tags = var.tags
-#     internal = each.value["internal"]
-#     sg_ingress_cidr = each.value["sg_ingress_cidr"]
-#     vpc_id = each.value["internal"]?local.vpc_id:var.default_vpc_id
-#     subnet_ids = each.value["internal"]?local.app_subnets:data.aws_subnets.subnets.ids
-#     sg_port = each.value["sg_port"]
+    for_each = var.alb
+    env = var.env
+    alb_type = each.value["lb_type"]
+    tags = var.tags
+    internal = each.value["internal"]
+    sg_ingress_cidr = each.value["sg_ingress_cidr"]
+    vpc_id = each.value["internal"]?local.vpc_id:var.default_vpc_id
+    subnet_ids = each.value["internal"]?local.app_subnets:data.aws_subnets.subnets.ids
+    sg_port = each.value["sg_port"]
 
-# }
+}
 
 
 module "docdb" {
